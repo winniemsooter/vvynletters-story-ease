aa9929a3d6d049cfe15d4035280219f626f74640
Timmy Adeyeloja <adefeyitimi@gmail.com>
2025-09-17 09:47:24 +0100

    feat: Add TypeScript types for blog feature

---
+++ src/types/blog.ts
@@ -0,0 +1,128 @@
+export interface WordPressPost {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: {
+    rendered: string;
+  };
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+    protected: boolean;
+  };
+  excerpt: {
+    rendered: string;
+    protected: boolean;
+  };
+  author: number;
+  featured_media: number;
+  comment_status: string;
+  ping_status: string;
+  sticky: boolean;
+  template: string;
+  format: string;
+  meta: any[];
+  categories: number[];
+  tags: number[];
+  _links: {
+    self: Array<{ href: string }>;
+    collection: Array<{ href: string }>;
+    about: Array<{ href: string }>;
+    author: Array<{ embeddable: boolean; href: string }>;
+    replies: Array<{ embeddable: boolean; href: string }>;
+    "version-history": Array<{ count: number; href: string }>;
+    "predecessor-version": Array<{ id: number; href: string }>;
+    "wp:attachment": Array<{ href: string }>;
+    "wp:term": Array<{ taxonomy: string; embeddable: boolean; href: string }>;
+    curies: Array<{ name: string; href: string; templated: boolean }>;
+  };
+}
+
+export interface WordPressMedia {
+  id: number;
+  date: string;
+  slug: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  author: number;
+  media_type: string;
+  mime_type: string;
+  media_details: {
+    width: number;
+    height: number;
+    file: string;
+    sizes: {
+      [key: string]: {
+        file: string;
+        width: number;
+        height: number;
+        mime_type: string;
+        source_url: string;
+      };
+    };
+  };
+  source_url: string;
+  alt_text: string;
+}
+
+export interface WordPressCategory {
+  id: number;
+  count: number;
+  description: string;
+  link: string;
+  name: string;
+  slug: string;
+  taxonomy: string;
+  parent: number;
+}
+
+export interface WordPressTag {
+  id: number;
+  count: number;
+  description: string;
+  link: string;
+  name: string;
+  slug: string;
+  taxonomy: string;
+}
+
+export interface BlogCategory {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string;
+  count?: number;
+}
+
+export interface BlogTag {
+  id: number;
+  name: string;
+  slug: string;
+  description?: string;
+  count?: number;
+}
+
+export interface BlogPost {
+  id: number;
+  title: string;
+  content: string;
+  excerpt: string;
+  slug: string;
+  date: string;
+  author: number;
+  featuredImage?: string;
+  featuredImageAlt?: string;
+  categories: number[];
+  tags: number[];
+}
